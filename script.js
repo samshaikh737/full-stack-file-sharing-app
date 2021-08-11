ba9da7786c4686a9dca5d1e154321ba018f29ce9
sameer <techallyoublog@gmail.com>
2021-08-11 18:17:23 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const fileModel = require("./models/fileModel");
+const fs = require("fs");
+
+const connectDB = require("./config/db");
+connectDB();
+
+async function fetchData() {
+    const pastDate = new Date(Date.now() -24 * 60 * 60 * 1000);
+    const files = await fileModel.find({ createdAt : { $lt : pastDate } });
+
+    if (files.length){
+        for(const file of files){
+            
+            try {
+                fs.unlinkSync(file.path);
+                await file.remove();
+                console.log(`successfully deleted ${file.filename}`);
+            } catch (error) {
+                console.log(`error while deleting file ${error}`);
+            }
+
+        }
+        console.log(`Job Done!`);
+    }
+}
+
+fetchData().then(process.exit);
